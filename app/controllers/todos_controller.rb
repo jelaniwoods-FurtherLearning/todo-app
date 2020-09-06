1d7d847461b12c50409b117cb347b7631b5037d5
Jelani Woods <jelani@firstdraft.com>
2020-09-05 20:16:20 -0500

    Add update Todo Route
    Update Pending component
    
    Add PendingItems component
    
    Update todos controller
    
    Update todos css

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,18 @@ class TodosController < ApplicationController
     render json: { completed: completed, pending: pending }
   end
 
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update_attributes!(todo_params)
+      render json: { message: "Todo Item updated successfully" }
+    else
+      render json: { message: "An error occured" }
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:id, :title, :completed)
+  end
 end
